6adf5027739155d7db6dd7e7d6a6145bfcd7a69d
Sheneka Woods <shenekawoods@Shenekas-MBP.attlocal.net>
2022-10-22 18:22:44 -0400

    first commit15

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,14 @@ from flask import render_template
 def Home():
     return render_template('index.html')
         
-@app.route('/MyFaves')
+@app.route('/MyTop5')
 def Top5():
     Fave = ['Caribbean', 'Japanese', 'Mexican', 'Korean', 'Mediterranean']
     return render_template('Base.html', Fave_food = Fave)
+
+@app.route('/MyFaves')
+def MyFaves():
+    Fave = ['Caribbean', 'Japanese', 'Mexican', 'Korean', 'Mediterranean']
+    return render_template('myfaves.html', Fave_food = Fave)
+        
         
